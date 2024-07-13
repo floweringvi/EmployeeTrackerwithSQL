fa28a11fc960d95c9546f48b00b7d78ca0d49666
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-12 21:46:17 -0400

    fixed schema.sql bugs

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,25 @@ CREATE DATABASE employee_db;
 \c employee_db
 
 CREATE TABLE department (
-    department_id SERIAL PRIMARY KEY
+    department_id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 
-)
+);
 
 CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
+    role_id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INTEGER,
    FOREIGN KEY (department_id)REFERENCES department(department_id)
-)
+);
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL, 
-    role_id INTEGER NOT NULL, 
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
     manager_id INTEGER 
     --This references a role that may or may not exist
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department(name)
+VALUES ('Human Resources'),
+       ('IT'),
+       ('Sales'), 
+       ('Marketing'), 
+       ('Legal');
+
+
+INSERT INTO role (title, salary)
+VALUES ('Manager', 45.000), 
+       ('Tech Support', 80.000),
+       ('Salesman', 75.000),
+       ('Brand Specialist', 65.000),
+       ('Lawyer', 98.000);
+
+
+INSERT INTO employee(first_name, last_name)
+VALUES ('John', 'Smith'),
+       ('Ronan', 'Noah'),
+       ('Danny', 'Nathaniel'),
+       ('Tallulah', 'Bankhead'),
+       ('Marnie', 'Wren');
