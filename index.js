4097cfa3f1345821079b852b27384973807e8fc7
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-12 21:22:53 -0400

    added question prompt

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const inquirer = require('inquirer')
 
-questions = [
-    
-]
+question = [
+    {
+        type: list,
+        name: inquiry,
+        message: "What would you like to do?", 
+        choices: ["View all Employees","Add Employee", "Update Employee Role","View All Roles","Add Role", "View All Departments", "Add Department"]
+
+     },
+]; 
