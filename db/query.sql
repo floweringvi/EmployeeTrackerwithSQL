9e738e057a3ac7ab22611098daccc92d3a651ad4
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-31 21:16:34 -0400

    here

--- db/query.sql
+++
@@ -1,5 +0,0 @@
---View department is just department table
-
---View all roles, shows everything under role plus department it belongs to
-
---When looking at all employees we want all the employee data, with department name subbed in and salary added
